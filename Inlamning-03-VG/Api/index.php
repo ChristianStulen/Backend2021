5a55ec22daccdf8ee783ffea2117360568251799
Christian Stulen <73221289+ChristianStulen@users.noreply.github.com>
2021-01-28 09:57:36 +0100

    Update index.php

--- Inlamning-03-VG/Api/index.php
+++ Inlamning-03-VG/Api/index.php
@@ -8,70 +8,80 @@ $diskArray = array(
         "title"=>"Beatles, Help",
         "picture"=> "BeatlesHelp.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>2,
         "title"=>"Beatles, For Sale",
         "picture"=> "BeatlesForSale.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>3,
         "title"=>"Djungelboken del 2",
         "picture"=> "DjungelbokenDel2.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>4,
         "title"=>"Du är den ende",
         "picture"=> "DuArDenEnde.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>5,
         "title"=>"Karl-Bertil Jonssons Julafton",
         "picture"=> "KarlBertilJonssonsJulafton.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>6,
         "title"=>"StatusQuo",
         "picture"=> "StatusQuo.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>7,
         "title"=>"Sven-Ingvars",
         "picture"=> "Sven-Ingvars.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>8,
         "title"=>"Världens bästa Karlsson",
         "picture"=> "VarldensBastaKarlsson.jpeg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>9,
         "title"=>"Lady och Lufsen",
         "picture"=> "LadyochLufsen.jpg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     ),
     array(
         "id"=>10,
         "title"=>"Anitas och Televinkens Trafikskiva",
         "picture"=> "AnitasTrafikskiva.jpg",
         "description"=>"Beskrivande text om skivan. Den är kanon!",
-        "price"=>"200kr"
+        "price"=>200,
+        "stock"=>rand(0,50)
     )
 );
 
